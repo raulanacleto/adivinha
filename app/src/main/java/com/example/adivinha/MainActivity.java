88a068530e0f77123a28708bc10c83f91b901ffc
Raul Anacleto <iraul@msn.com>
2019-11-12 10:04:05 -0300

    'commit.inicial'

---
+++ app/src/main/java/com/example/adivinha/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.adivinha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.botaoJogarId);
+        textoResultado = findViewById(R.id.resultadoId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+                textoResultado.setText("Número gerado:"+ numeroAleatorio);
+            }
+        });
+    }
+}
